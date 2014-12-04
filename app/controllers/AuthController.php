1a4c29f7ab886ae6df1721daa23e3b385a1f9418
Joel Francisco <joeld.fran@hotmail.com>
2014-12-04 18:38:22 +0000

    dices controller test

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -8,47 +8,47 @@ class AuthController extends BaseController {
 		return View::make('users.login');
 	}
 
-	// public function postLogin()
-	// {
-	// 	$input = Input::only(['username', 'password']);
-	// 	$validator = Validator::make($input, User::$login_rules);
+	public function postLogin()
+	{
+		$input = Input::only(['username', 'password']);
+		$validator = Validator::make($input, User::$login_rules);
 
-	// 	if($validator->fails()) {
-	// 		return Redirect::to('login')->withErrors($validator)->withInput(Input::only('username'));
-	// 	}
-
-	// 	if(Auth::attempt(['username' => $input['username'], 'password' => $input['password']])) 
-	// 	{
-	// 		return Redirect::to('/')->with('success','Wellcome '.Auth::user()->username);
-	// 	} 
-	// 	elseif(Auth::attempt(['email' => $input['username'], 'password' => $input['password']])) 
-	// 	{
-	// 		return Redirect::to('/')->with('success','Wellcome '.Auth::user()->username);
-	// 	}
-
-	// 	return Redirect::to('login')->withErrors('Wrong Username/Email and Password combination.')->withInput(Input::only('username'));
-	// }
-
-	public function postLogin() {
-		$creds_name = [
-		'username' => DecryptedInput::get('username'),
-		'password'  => DecryptedInput::get('password'),
-		];
-		$creds_email = [
-		'email' => DecryptedInput::get('username'),
-		'password'  => DecryptedInput::get('password'),
-		];
-
-		$response = new Yatzhee\Dto\Responses\LoginResponse;
-
-		if(Auth::attempt($creds_name, false) || Auth::attempt($creds_email, false)){
-			$response->loginResult = Yatzhee\Dto\Responses\LoginResponse::LOGIN_SUCCESS;
-			$response->expire = Auth::user()->tariffExpire;
-			return json_encode($response);
+		if($validator->fails()) {
+			return Redirect::to('login')->withErrors($validator)->withInput(Input::only('username'));
 		}
-		$response->loginResult = Yatzhee\Dto\Responses\LoginResponse::LOGIN_FAIL;
-		return json_encode($response);
+
+		if(Auth::attempt(['username' => $input['username'], 'password' => $input['password']])) 
+		{
+			return Redirect::to('/')->with('success','Wellcome '.Auth::user()->username);
+		} 
+		elseif(Auth::attempt(['email' => $input['username'], 'password' => $input['password']])) 
+		{
+			return Redirect::to('/')->with('success','Wellcome '.Auth::user()->username);
+		}
+
+		return Redirect::to('login')->withErrors('Wrong Username/Email and Password combination.')->withInput(Input::only('username'));
 	}
+
+	// public function postLogin() {
+	// 	$creds_name = [
+	// 	'username' => DecryptedInput::get('username'),
+	// 	'password'  => DecryptedInput::get('password'),
+	// 	];
+	// 	$creds_email = [
+	// 	'email' => DecryptedInput::get('username'),
+	// 	'password'  => DecryptedInput::get('password'),
+	// 	];
+
+	// 	$response = new Yatzhee\Dto\Responses\LoginResponse;
+
+	// 	if(Auth::attempt($creds_name, false) || Auth::attempt($creds_email, false)){
+	// 		$response->loginResult = Yatzhee\Dto\Responses\LoginResponse::LOGIN_SUCCESS;
+	// 		$response->expire = Auth::user()->tariffExpire;
+	// 		return json_encode($response);
+	// 	}
+	// 	$response->loginResult = Yatzhee\Dto\Responses\LoginResponse::LOGIN_FAIL;
+	// 	return json_encode($response);
+	// }
 
 
 
--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,25 @@
 
 var app = angular.module('app', ['appControllers'],
 	function($interpolateProvider) 
-    {
-      $interpolateProvider.startSymbol('[[');
-      $interpolateProvider.endSymbol(']]');
-    }
-);
+	{
+		$interpolateProvider.startSymbol('[[');
+		$interpolateProvider.endSymbol(']]');
+	}
+	);
+
+app.controller('DicesController', ['$scope', function($scope){
+	$scope.dices[{val:1,saved:false},
+				{val:1,saved:false},
+				{val:1,saved:false},
+				{val:1,saved:false},
+				{val:1,saved:false}];
+
+	$scope.result['sum'=>function(){
+		var val = 0;
+		for(var dices of $scope.dices){
+			val+=dices.val;
+		}
+		return val;
+	}];
+	
+}])
